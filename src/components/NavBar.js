db0841867b77a7e2f519bed403fdfad646bf6570
kenneth <kennjorothuo@gmail.com>
2024-10-30 11:06:51 +0300

    initiall commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,10 @@ import React from "react";
 function NavBar() {
   // update the JSX being returned!
   return <nav>NavBar</nav>;
+  <nav>
+    <a href="#home"></a>
+    <a href="#about"></a>
+  </nav>
 }
 
 export default NavBar;
